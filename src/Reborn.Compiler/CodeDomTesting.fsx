b0f0975cb56585447405ac59b70f2c255de1df61
Torbjørn Marø <torbjorn.maro@gmail.com>
2014-12-12 23:24:21 +0100

    CodeDom DSL

--- src/Reborn.Compiler/CodeDomTesting.fsx
+++ src/Reborn.Compiler/CodeDomTesting.fsx
@@ -1,69 +1,27 @@
-﻿open System
+﻿//#load "AST.fs"
+//#load "Parsing.fs"
+#load "Compiling.fs"
 
-// CHECK WHICH PROVIDERS ARE AVAILABLE
+open System
+open Reborn.Compiling
 
-let compilers = CodeDom.Compiler.CodeDomProvider.GetAllCompilerInfo()
-compilers |> Array.iter (fun c -> c.GetLanguages() |> printfn "%A")
+let csc = createCSharpProvider ()
+let parms = getCompilerParams ()
 
-// COMPILE FROM SOURCE WITH OPTIONS!
+let main = ``method`` "Main"
+           |> returntype "System.Void"
+           |> attributes publicStatic
 
-let csc = CodeDom.Compiler.CodeDomProvider.CreateProvider "c#"
-let ``params`` = new CodeDom.Compiler.CompilerParameters()
-``params``.GenerateExecutable <- true
-``params``.GenerateInMemory <- false
-``params``.IncludeDebugInformation <- true
-//``params``.MainClass = ""
-``params``.OutputAssembly = ".\\test.exe"
-//``params``.ReferencedAssemblies.Add ""
-``params``.TreatWarningsAsErrors <- true
-//``params``.CompilerOptions <- "" // commandline arguments
-let result = csc.CompileAssemblyFromSource(``params``, [|
-                                                            @"class Foo { 
-                                                                static void Main(){
-                                                                }
-                                                              }"
-                                                        |])
+let (|..>) e f = f [e]
 
-printfn "Compiler returned %A" result.NativeCompilerReturnValue
-for e in result.Errors do
-    printfn "(%A) Error line %A col %A: %s" 
-        e.ErrorNumber e.Line e.Column e.ErrorText
-printfn "Assembly at %s" result.PathToAssembly
-for o in result.Output do
-    printfn "Compiler said: %s" o
+let compileUnit =
+    main
+    |..> wrapInClass "MyClass" nonPublic
+    |..> wrapInNamespace "MyNamespace"
+    |..> wrapInCompileUnit
 
-// CODEDOM TEST
-
-let ns = CodeDom.CodeNamespace "MyNamespace"
-let cl = CodeDom.CodeTypeDeclaration "MyClass"
-cl.TypeAttributes <- Reflection.TypeAttributes.NotPublic
-
-//let fi = CodeDom.CodeMemberField("System.Int32", "_field")
-//fi.Attributes <- CodeDom.MemberAttributes.Private
-
-//cl.Members.Add fi
-
-let main = CodeDom.CodeMemberMethod()
-main.Attributes <- CodeDom.MemberAttributes.Public ||| CodeDom.MemberAttributes.Static
-main.ReturnType <- CodeDom.CodeTypeReference "System.Void"
-main.Name <- "Main"
-
-cl.Members.Add main
-
-ns.Types.Add cl
-
-let compileUnit = CodeDom.CodeCompileUnit()
-compileUnit.Namespaces.Add ns
-
-let result2 = csc.CompileAssemblyFromDom(``params``, [|compileUnit|])
-
-printfn "Compiler returned %A" result2.NativeCompilerReturnValue
-for e in result2.Errors do
-    printfn "(%A) Error line %A col %A: %s" 
-        e.ErrorNumber e.Line e.Column e.ErrorText
-printfn "Assembly at %s" result2.PathToAssembly
-for o in result2.Output do
-    printfn "Compiler said: %s" o
+csc.CompileAssemblyFromDom(parms, [|compileUnit|])
+|> printCompilerResult
 
 
 
--- src/Reborn.Compiler/Compiling.fs
+++ src/Reborn.Compiler/Compiling.fs
@@ -3,19 +3,52 @@
 open System
 open System.CodeDom
 
+let createCSharpProvider () =
+    CodeDom.Compiler.CodeDomProvider.CreateProvider "c#"
+
+let getCompilerParams () =
+    let ``params`` = new CodeDom.Compiler.CompilerParameters()
+    ``params``.GenerateExecutable <- true
+    ``params``.GenerateInMemory <- false
+    ``params``.IncludeDebugInformation <- true
+    //``params``.MainClass = ""
+    ``params``.OutputAssembly <- ".\\test.exe"
+    //``params``.ReferencedAssemblies.Add ""
+    ``params``.TreatWarningsAsErrors <- true
+    //``params``.CompilerOptions <- "" // commandline arguments
+    ``params``
+
+let printCompilerResult (res:Compiler.CompilerResults) =
+    for e in res.Errors do
+        printfn "(%A) Error line %A col %A: %s" 
+            e.ErrorNumber e.Line e.Column e.ErrorText
+    printfn "Assembly at %s" res.PathToAssembly
+    printfn (if res.NativeCompilerReturnValue = 0
+             then "COMPILATION SUCCESSFUL"
+             else "COMPILATION FAILED!")
+
 let publicStatic = MemberAttributes.Public ||| MemberAttributes.Static
 let nonPublic = Reflection.TypeAttributes.NotPublic
 
-let ns name = CodeNamespace name
+let wrap x elemAction elems =
+    let elemAction' = elemAction x
+    elems |> List.iter elemAction'
+    x
 
-let klass name attrs =
-    let k = CodeTypeDeclaration name
-    k.TypeAttributes <- attrs
-    k
+let wrapInNamespace name =
+    wrap (CodeNamespace name)
+         (fun ns t -> ns.Types.Add t |> ignore)
+        
+let wrapInCompileUnit namespaces =
+    namespaces
+    |> wrap (CodeCompileUnit())
+            (fun cu n -> cu.Namespaces.Add n |> ignore)
 
-let addMember m (klass:CodeTypeDeclaration) =
-    klass.Members.Add m |> ignore
-    klass
+let wrapInClass name attrs members =
+    let c = CodeTypeDeclaration name
+    c.TypeAttributes <- attrs
+    c.Attributes <- MemberAttributes.Static // No effect ?
+    members |> wrap c (fun c m -> c.Members.Add m |> ignore)
 
 let ``method`` name =
     let m = CodeMemberMethod ()
@@ -29,8 +62,3 @@ let returntype (t:string) (m:CodeMemberMethod) =
 let attributes (a:MemberAttributes) (m:CodeMemberMethod) =
     m.Attributes <- a
     m
-
-klass "Foo" nonPublic
-|> addMember (``method`` "Main"
-              |> returntype "System.Void"
-              |> attributes publicStatic)
